dc68349413dabafb4f1a4db17135898513262a71
arrbsd <arriah@gmail.com>
2024-12-03 21:45:42 +0300

    Готовое ДЗ

--- for_challenges.py
+++ for_challenges.py
@@ -55,3 +55,47 @@ groups = [
     ['Вася', 'Маша', 'Саша', 'Женя'],
 ]
 # ???
+
+
+# Вывод имени на каждой строке
+def get_people_names(people_names):
+    for people_name in people_names:
+        print (f'{people_name}')
+
+
+# Вывод имени с указанием количество символов
+def get_count_letters(people_names):
+    for people_name in people_names:
+        print(f'{people_name} - {len(people_name)}')
+
+
+# Вывод пола напротив имени
+def get_sex_people(people_sex):
+    for people_gender in people_sex:
+        if is_male[people_gender]:
+            print(f'{people_gender} - Мужской')
+        else:
+            print(f'{people_gender} - Женский')
+
+
+# вывод групп и количество учеников
+def get_count_people (people_groups):
+    print(f'Общее количество групп - {len(people_groups)}')
+    count = 0
+    for one_group in people_groups:
+        count += 1
+        print(f'Количество учеников в группе {count} - {len(one_group)}')
+
+
+# Вывод имен учеников в каждой группе
+def get_name_people(people_groups):
+    count = 0
+    for one_group in people_groups:
+        count += 1
+        print(f'Имена учащихся в группе {count} - {', '.join(one_group)}')
+
+get_people_names(names)
+get_count_letters(names)
+get_sex_people(is_male)
+get_count_people (groups)
+get_name_people(groups)
