1019795b28b49882779a20b3b4f2ab4030db7e56
arrbsd <arriah@gmail.com>
2024-12-04 01:26:03 +0300

    Готовое ДЗ

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,4 +1,3 @@
-# Новое задание
 # Задание 1
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика
 # Пример вывода:
@@ -61,12 +60,12 @@ school_students = [
 # Класс 2a: девочки 2, мальчики 0 
 # Класс 2б: девочки 0, мальчики 2
 
-school = [
+school_one = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
     {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
     {'class': '2в', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
 ]
-is_male = {
+is_male_one = {
     'Олег': True,
     'Маша': False,
     'Оля': False,
@@ -82,11 +81,11 @@ is_male = {
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
 
-school = [
+school_two = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
     {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
 ]
-is_male = {
+is_male_two = {
     'Маша': False,
     'Оля': False,
     'Олег': True,
@@ -94,3 +93,75 @@ is_male = {
 }
 # ???
 
+
+from collections import Counter
+
+def new_list_names(student_names):
+    # создание нового списка имен
+    list_names = []
+    for student in student_names:
+        list_names.append(student['first_name'])
+    return list_names
+
+
+def most_name_common(student_names):
+    names_count = new_list_names(student_names)
+    return max(names_count, key=names_count.count)
+
+
+# вывод количества повторений каждого имени
+names_count = new_list_names(students)
+for name, count_name in Counter(names_count).items():
+     print (f'{name}: {count_name}')
+print(Counter(names_count))
+
+
+# вывод самого частого имени
+print(most_name_common(students))
+
+
+# вывод самых частых имен в каждом классе
+count = 0
+for one_class in school_students:
+    count += 1
+    print(f'Самое частое имя в классе {count}. {most_name_common(one_class)}')
+
+
+# считаем мальчиков и девочек в каждом классе и создаем новый словарь
+def count_gender(school, is_male):
+    new_class = {}
+    for one_class in school:
+        girls = 0
+        boys = 0
+        for student in one_class['students']:
+            name = student['first_name']
+            if is_male[name]:
+                boys += 1
+            else:
+                girls += 1
+        new_class[one_class['class']] = {'Male': boys, 'Female': girls}
+    return new_class
+
+
+# выводим количество мальчиков и количество девочек по каждому классу
+for classroom, class_gender in count_gender(school_one, is_male_one).items():
+    print(f'В классе {classroom} мальчиков {class_gender["Male"]} девочек {class_gender["Female"]}')
+
+
+# ищем в каком классе больше мальчиков, а в каком больше девочек
+genders_in_school = count_gender(school_two, is_male_two)
+max_boys=0
+max_boys_in_class = 0
+max_girls = 0
+max_girls_in_class = 0
+for gender_class, gender in genders_in_school.items():
+    print(gender_class)
+    if gender['Male'] > max_boys:
+        max_boys = gender['Male']
+        max_boys_in_class = gender_class
+    if gender['Female'] > max_girls:
+        max_girls = gender['Female']
+        max_girls_in_class = gender_class
+
+print(f'В классе {max_girls_in_class} больше всего девочек')
+print(f'В классе {max_boys_in_class} больше всего мальчиков')
